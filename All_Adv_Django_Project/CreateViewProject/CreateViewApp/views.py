b98a49b6831920340acc56eef18e3054f98246b2
psarangi550 <psarangi50@gmail.com>
2021-10-18 00:32:58 +0530

    Modified Commits

--- All_Adv_Django_Project/CreateViewProject/CreateViewApp/views.py
+++ All_Adv_Django_Project/CreateViewProject/CreateViewApp/views.py
@@ -3,14 +3,21 @@ from django.views.generic.edit import CreateView
 from .models import Employee
 from django import forms
 from .forms import EmployeeForm
+from django.contrib import messages
+from django.utils.text import slugify
+from django.utils.translation import  gettext,gettext_lazy as _
+
 # Create your views here.
 class EmpCreateView(CreateView):
     # model=Employee
     form_class=EmployeeForm
     # fields="__all__"
-    success_url="/thanks/"
+    # success_url="/thanks/"
     template_name="CreateViewApp/employee_form.html"
     # def get_form(self):
     #     form=super().get_form()
     #     form.fields["ename"].widget=forms.TextInput(attrs={"placeholder":"Enter Your Name"})
     #     return form#return form object
+    def post(self,request):
+        messages.error(request, "object inserted successfully")
+        return render(request, "CreateViewApp/thanks.html", )
--- All_Adv_Django_Project/CreateViewProject/CreateViewProject/urls.py
+++ All_Adv_Django_Project/CreateViewProject/CreateViewProject/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from CreateViewApp import views
 from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('thanks/', TemplateView.as_view(template_name="CreateViewApp/thanks.html"),name="thanks"),
     path("create/", views.EmpCreateView.as_view(),name="empcreate"),
-]
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
