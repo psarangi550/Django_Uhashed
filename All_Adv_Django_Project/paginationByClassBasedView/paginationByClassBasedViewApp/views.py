2100a499c12e7e4129303badf1252f434319a26f
psarangi550 <psarangi50@gmail.com>
2021-10-24 11:27:24 +0530

    Modified Commits

--- All_Adv_Django_Project/paginationByClassBasedView/paginationByClassBasedViewApp/views.py
+++ All_Adv_Django_Project/paginationByClassBasedView/paginationByClassBasedViewApp/views.py
@@ -11,12 +11,21 @@ class MyListView(ListView):
     paginate_by=2
     paginate_orphans=1
 
-    def get_context_data(self,*args, **kwargs):
+    # def get_context_data(self,*args, **kwargs):
+    #     try:
+    #         return super().get_context_data(*args, **kwargs)
+    #     except Http404:
+    #         self.kwargs["page"]=1
+    #         return super().get_context_data(*args, **kwargs)
+
+    def paginate_queryset(self,queryset,page_size):
         try:
-            return super().get_context_data(*args, **kwargs)
+            return super().paginate_queryset(queryset,page_size)
         except Http404:
             self.kwargs["page"]=1
-            return super().get_context_data(*args, **kwargs)
+            return super().paginate_queryset(queryset,page_size)
+
+
 
 
 
